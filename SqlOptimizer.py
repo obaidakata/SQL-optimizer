14663850e2794d4122ca4f8b0395cbc20fd0ee40
Obaida kata <obaida_18@me.com>
2020-12-20 14:39:03 +0200

    Rules 4, 6 works with nested array

--- SqlOptimizer.py
+++ SqlOptimizer.py
@@ -43,33 +43,34 @@ class SqlOptimizer:
         self.__QueryTree = [pi, sigma, cartesian, tables]
         return fromSubQuery
 
-    def __getOperatorConditionAndOperand(self, i_OperatorName, i_NextOperatorName):
-        if i_NextOperatorName == "CARTESIAN":
-            res = self.__getOperatorConditionAndOperand2(i_OperatorName, i_NextOperatorName)
-            tablesIndex = res[2]
-            res[1] = self.__QueryTree[tablesIndex]
-            return res
-        else:
-            return self.__getOperatorConditionAndOperand2(i_OperatorName, i_NextOperatorName)
+    # def __getOperatorConditionAndOperand(self, i_OperatorName, i_NextOperatorName):
+    #     if i_NextOperatorName == "CARTESIAN":
+    #         res = self.__getOperatorConditionAndOperand2(i_OperatorName, i_NextOperatorName)
+    #         tablesIndex = res[2]
+    #         res[1] = self.__QueryTree[tablesIndex]
+    #         self.__QueryTree.pop()
+    #         return res
+    #     else:
+    #         return self.__getOperatorConditionAndOperand2(i_OperatorName, i_NextOperatorName)
 
 
-    def __getOperatorConditionAndOperand2(self, i_OperatorName, i_NextOperatorName):
-        toReturn = None
-        treeLength = len(self.__QueryTree)
-        for i in range(treeLength):
-            subQuery = self.__QueryTree[i]
-            if isinstance(subQuery, str) and subQuery.startswith(i_OperatorName):
-                nextSubQuery = self.__QueryTree[i + 1] if (i + 1 < treeLength) else None
-                if (nextSubQuery is not None) and (i_NextOperatorName is None) or (
-                        nextSubQuery.startswith(i_NextOperatorName)):
-                    # Pop the operator and its operands
-                    self.__QueryTree.pop(i)
-                    self.__QueryTree.pop(i)
-                    condition = self.__getSub(subQuery, self.__SquareBrackets)
-                    toReturn = [condition, nextSubQuery, i]
-                    break
-
-        return toReturn
+    # def __getOperatorConditionAndOperand2(self, i_OperatorName, i_NextOperatorName):
+    #     toReturn = None
+    #     treeLength = len(self.__QueryTree)
+    #     for i in range(treeLength):
+    #         subQuery = self.__QueryTree[i]
+    #         if isinstance(subQuery, str) and subQuery.startswith(i_OperatorName):
+    #             nextSubQuery = self.__QueryTree[i + 1] if (i + 1 < treeLength) else None
+    #             if (nextSubQuery is not None) and (i_NextOperatorName is None) or (
+    #                     nextSubQuery.startswith(i_NextOperatorName)):
+    #                 # Pop the operator and its operands
+    #                 self.__QueryTree.pop(i)
+    #                 self.__QueryTree.pop(i)
+    #                 condition = self.__getSub(subQuery, self.__SquareBrackets)
+    #                 toReturn = [condition, nextSubQuery, i]
+    #                 break
+    #
+    #     return toReturn
 
     def _getTheProperJoinFormat(self, i_Condition):
         format = "TJOIN[{0}]({1})"
@@ -92,22 +93,34 @@ class SqlOptimizer:
 
 
     def __str__(self):
-        return self.__toString()
+        return self.__toString(self.__QueryTree)
 
-    def __toString(self):
-        toReturn = self.__QueryTree[0]
-        for x in self.__QueryTree[1:]:
-            toReturn += "("
+    def __toString(self, listToPrint):
+        toReturn = ""
+        close = 0
+        for x in listToPrint:
             if isinstance(x, str):
                 toReturn += x
+                if self.__isOperator(x):
+                    toReturn += "("
+                    close = close + 1
             else:
-                for y in x:
-                    toReturn += y
+                inside = self.__toString(x)
+                toReturn += inside
 
-        for _ in self.__QueryTree[1:]:
+
+        for _ in range(close):
             toReturn += ")"
         return toReturn
 
+    def __isOperator(self, stirngToCheck):
+        if isinstance(stirngToCheck, str):
+            res = stirngToCheck.startswith("PI")
+            res = res or stirngToCheck.startswith("SIGMA")
+            res = res or stirngToCheck.startswith("CARTESIAN")
+            res = res or stirngToCheck.startswith("NJOIN")
+            return res
+        return False
     def setQuery(self, i_Query):
         self.__buildTree(i_Query)
         print(self)
@@ -118,18 +131,19 @@ class SqlOptimizer:
         elif i_Rule == self.__options[1]:
             self.__rule6()
         elif i_Rule == self.__options[2]:
-            self.__rule4()
+            self.__rule42()
         elif i_Rule == self.__options[3]:
             self.__rule4a()
         elif i_Rule == self.__options[4]:
             self.__rule5a()
         else:
             print("Error")
-        optimizedQuery = self.__toString()
+        optimizedQuery = self.__toString(self.__QueryTree)
         return optimizedQuery
 
     def __rule11b(self):
-        res = self.__getOperatorConditionAndOperand("SIGMA", "CARTESIAN")
+        #TODO: FIX
+        res = self.__getOperatorConditionAndOperand(self.__QueryTree, "SIGMA", "CARTESIAN")
         if res is not None:
             tables = res[1]
             joinFormat = self._getTheProperJoinFormat(res[0])
@@ -137,16 +151,52 @@ class SqlOptimizer:
             self.__QueryTree.append(condition)
 
     def __rule6(self):
-        res = self.__getOperatorConditionAndOperand("SIGMA", "CARTESIAN")
+        res = self.__getOperatorConditionAndOperand(self.__QueryTree, "SIGMA", "CARTESIAN")
         if res is not None:
-            firstTable = res[1][0]
-            secondTable = res[1][1]
-            sigma = "SIGMA[{0}]({1})".format(res[0], firstTable)
-            toInsert = [sigma, secondTable]
-            self.__QueryTree.pop()
-            self.__QueryTree.append("CARTESIAN")
-            self.__QueryTree.append(toInsert)
+            tempArray = self.__QueryTree
+            for i in res:
+                tempArray = tempArray[i]
+            sigma = tempArray
+            cartisianIndex = res[-1] + 1
+            cartisianTablesIndex = cartisianIndex + 1
+            cartisiainTables = self.__QueryTree[cartisianTablesIndex]
+            toInsert = ["CARTESIAN", [[sigma, cartisiainTables[0]], cartisiainTables[1]]]
+            self.__QueryTree.pop(cartisianIndex)
+            self.__QueryTree.pop(cartisianIndex) # Pop out catisian tables
+            toInsert.reverse()
+            self.__QueryTree = self.insertIntoNestedArray(self.__QueryTree, res, toInsert)
 
+
+
+    def __rule42(self):
+        res = self.__findSigmaWithAndCondition(self.__QueryTree)
+        if res is not None:
+            tempArray = self.__QueryTree
+            for i in res:
+                tempArray = tempArray[i]
+            sigma = tempArray
+            sigmaCondition = self.__getSub(sigma, self.__SquareBrackets)
+            if sigmaCondition is not None and "AND" in sigmaCondition:
+                splitted_sigmaCondition = sigmaCondition.split("AND", 1)
+                sec1 = splitted_sigmaCondition[0].strip()
+                sec2 = splitted_sigmaCondition[1].strip()
+                newSigma1 = "SIGMA[{0}]".format(sec1)
+                newSigma2 = "SIGMA[{0}]".format(sec2)
+                toInsert = [newSigma2, newSigma1]
+                self.__QueryTree = self.insertIntoNestedArray(self.__QueryTree, res, toInsert)
+
+    def insertIntoNestedArray(self, nestedArray, indexs, toInsert):
+        newArray = nestedArray
+        for i in indexs:
+            if isinstance(newArray[i], str):
+                newArray.pop(i)
+                for y in toInsert:
+                    newArray.insert(i, y)
+            else:
+                indexs.pop(0)
+                subArray = self.insertIntoNestedArray(newArray[i], indexs, toInsert)
+                newArray[i] = subArray
+        return newArray
 
 
     def __rule4(self):
@@ -169,8 +219,8 @@ class SqlOptimizer:
             self.__QueryTree.insert(i + 1, newSigma2)
 
     def __rule4a(self):
-        # TODO: What if thrre is 3 sigma witch one i should swap
-        res = self.__getOperatorConditionAndOperand("SIGMA", "SIGMA")
+        # TODO: What if there are 3 sigma witch one i should swap
+        res = self.__getOperatorConditionAndOperand(self.__QueryTree, "SIGMA", "SIGMA")
         if res is not None:
             firstSigmaCondition = res[0]
             secondSigmaCondition = self.__getSub(res[1], self.__SquareBrackets)
@@ -181,7 +231,7 @@ class SqlOptimizer:
             self.__QueryTree.insert(oldSigmaIndex + 1, newSigma2)
 
     def __rule5a(self):
-        res = self.__getOperatorConditionAndOperand("PI", "SIGMA")
+        res = self.__getOperatorConditionAndOperand(self.__QueryTree, "PI", "SIGMA")
         if res is not None:
             piCondition = res[0]
             SigmaCondition = self.__getSub(res[1], self.__SquareBrackets)
@@ -190,3 +240,38 @@ class SqlOptimizer:
             index = res[2]
             self.__QueryTree.insert(index, sigma)
             self.__QueryTree.insert(index + 1, pi)
+
+    def __findSigmaWithAndCondition(self, arrayToLookFor):
+        res = None
+        arrayLen = len(arrayToLookFor)
+        for i in range(arrayLen):
+            subQuery = arrayToLookFor[i]
+            if isinstance(subQuery, str):
+                if subQuery.startswith("SIGMA"):
+                    sigmaCondition = self.__getSub(subQuery, self.__SquareBrackets)
+                    if "AND" in sigmaCondition:
+                        res = [i]
+                        break
+            else:
+                res = [i] + self.__findSigmaWithAndCondition(subQuery)
+        return res
+
+    def __getOperatorConditionAndOperand(self, arrayToLookFor, i_OperatorName, i_NextOperatorName):
+        toReturn = None
+        arrayLen = len(arrayToLookFor)
+        for i in range(arrayLen):
+            subQuery = arrayToLookFor[i]
+            if isinstance(subQuery, str):
+                if subQuery.startswith(i_OperatorName):
+                    nextSubQuery = arrayToLookFor[i + 1] if (i + 1 < arrayLen) else None
+                    if nextSubQuery is not None:
+                        if isinstance(nextSubQuery, str) and nextSubQuery.startswith(i_NextOperatorName):
+                            # return i_OperatorName index
+                            toReturn = [i]
+                            break
+            else:
+                # return full path of nested i_OperatorName
+                toReturn = [i] + self.__getOperatorConditionAndOperand(subQuery, i_OperatorName, i_NextOperatorName)
+
+
+        return toReturn
